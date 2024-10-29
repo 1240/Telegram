b1d35d179d996598dc3abe7fd0b8623faeabb6bf
l24o <chuffey.1240@gmail.com>
2024-10-29 16:29:53 +0300

    h3110 w0r1d!

--- TMessagesProj/src/main/java/org/telegram/ui/MetaballViewFINAL.java
+++ TMessagesProj/src/main/java/org/telegram/ui/MetaballViewFINAL.java
@@ -1093,7 +1093,7 @@ public class MetaballViewFINAL extends View {
 
     public void startAnimation() {
         animator = ValueAnimator.ofFloat(0f, 1f);
-        animator.setDuration(1000);
+        animator.setDuration(600);
         animator.setInterpolator(new OvershootInterpolator(1.01f));
 
         animator.addUpdateListener(animation -> {
